6f8969568624c99efa27ef3d6bfc600558eaa64c
Dhananjayan P N <pndjay@gmail.com>
2021-11-29 05:59:22 -0600

    pagination

--- frontend/src/components/Tickets/Tickets.js
+++ frontend/src/components/Tickets/Tickets.js
@@ -8,6 +8,10 @@ import './Tickets.css';
 
 const Tickets = () => {
   const [tickets, setTickets] = useState(null);
+  const [prev, setPrev] = useState(null);
+  const [next, setNext] = useState(null);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(false);
   const navigate = useNavigate();
   
   useEffect(() => {
@@ -21,10 +25,60 @@ const Tickets = () => {
       navigate("/auth", { replace: true });
       return;
     }
+    if (prev || !next)
     axios.post("http://localhost:8080/alltickets",
       {subdomain, token, pagesize: 25})
     .then(response => {
+      console.log(response);
       setTickets(response.data.tickets);
+      setPrev(response.data.links.prev);
+      setNext(response.data.links.next);
+      setHasMore(response.data.meta.has_more);
+    })
+    .catch(err => {
+      console.log(err);
+      alert("Uh oh! Something went wrong! Try again later!")
+    })
+  }
+  
+  const getNext = () => {
+    let token = Cookies.get('token');
+    let subdomain = Cookies.get('subdomain');
+    if (!token) {
+      navigate("/auth", { replace: true });
+      return;
+    }
+    axios.post("http://localhost:8080/alltickets",
+      {subdomain, token, url: next})
+    .then(response => {
+      console.log(response);
+      setTickets(response.data.tickets);
+      setPrev(response.data.links.prev);
+      setNext(response.data.links.next);
+      setHasMore(response.data.meta.has_more);
+      setPage(page + 1);
+    })
+    .catch(err => {
+      console.log(err);
+      alert("Uh oh! Something went wrong! Try again later!")
+    })
+  }
+  
+  const getPrev = () => {
+    let token = Cookies.get('token');
+    let subdomain = Cookies.get('subdomain');
+    if (!token) {
+      navigate("/auth", { replace: true });
+      return;
+    }
+    axios.post("http://localhost:8080/alltickets",
+      {subdomain, token, url: prev})
+    .then(response => {
+      setTickets(response.data.tickets);
+      setPrev(response.data.links.prev);
+      setNext(response.data.links.next);
+      setPage(page - 1);
+      setHasMore(response.data.meta.has_more);
     })
     .catch(err => {
       console.log(err);
@@ -48,7 +102,7 @@ const Tickets = () => {
         <Typography variant="caption" style={{textAlign: "center", marginBottom: 20}} gutterBottom display="block">
           {`${Cookies.get('subdomain')}.zendesk.com`}
         </Typography>
-        <Button style={{position: "absolute", top: 0, bottom: 0}} variant="outlined" onClick={signOut}>
+        <Button style={{position: "absolute", right: 0, top: 10}} variant="outlined" onClick={signOut}>
           Sign Out
         </Button>
       </div>
@@ -67,6 +121,17 @@ const Tickets = () => {
           </div>)
         }
       </div>
+      <div style={{display:"flex", margin: "25px 0px", alignItems: "center", justifyContent: "center"}}>
+          {prev && <Button variant="outlined" onClick={getPrev} disabled={page === 1}>
+            Previous Page
+          </Button>}
+          <Typography variant="overline" style={{textAlign: "center", margin: "10px 20px"}}  component="div">
+            Page: {page}
+          </Typography>
+          {next && <Button variant="outlined" onClick={getNext} disabled={!hasMore}>
+            Next Page
+          </Button>}
+        </div>
     </div>
   );
 }
